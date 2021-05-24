e61b8c8f8c2ff6c4b8b246238c3bd8f4e782d935
thangarajdeveloper <84706944+thangarajdeveloper@users.noreply.github.com>
2021-05-24 14:33:59 +0530

    urlfile updated

--- blog_proj/blog_proj/urls.py
+++ blog_proj/blog_proj/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
     # adding blog app urls
     path('', include('blogapp.urls')),
 ]
